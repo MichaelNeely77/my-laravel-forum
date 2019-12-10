b89e17cb3b75c442729210c36e78b200c01688f6
Michael Neely <michael@michaelneely.net>
2019-12-10 00:53:41 -0500

    added the 'Add Discussion button and more

--- database/migrations/2019_12_10_050203_create_channels_table.php
+++ database/migrations/2019_12_10_050203_create_channels_table.php
@@ -16,7 +16,7 @@ class CreateChannelsTable extends Migration
         Schema::create('channels', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
