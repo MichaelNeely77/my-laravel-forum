5e970a2697fcceb6b07ba690389a479d261eba4f
Michael Neely <michael@michaelneely.net>
2019-12-10 23:51:20 -0500

    Added directory pages to the discussion forum

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,13 @@
 
 
         <div class="d-flex justify-content-end mb-2">
-                <a href="" class="btn btn-success">Add Discussion</a>
+        <a href="{{ route('discussion.create') }}" class="btn btn-success">Add Discussion</a>
             </div>
     <div class="card">
         <div class="card-header">Dashboard</div>
 
         <div class="card-body">
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            You are logged in!
+            Dashboard
         </div>
     </div>
 
