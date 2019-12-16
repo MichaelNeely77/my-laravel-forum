d756b41094ddad09b7acb09020a9127d4032655a
Michael Neely <michael@michaelneely.net>
2019-12-15 23:54:42 -0500

    Saved UsersController

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    public function notifications() 
+    {
+        
+        // mark all as read
+
+        // display all notifications
+
+    }
 }
